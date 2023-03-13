7d21ccb7e55df7173dbfdfa960036d52e52b9183
Otavio Cantareli <otavio_cantareli@outlook.com>
2023-03-13 18:27:02 -0300

    feat: Add routes
    Add new routes: 'view' and 'create'

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Ayooo"))
 }
 
+func view(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Viewing..."))
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/view", view)
+	mux.HandleFunc("/create", create)
 
 	log.Print("Starting on :4000")
 	err := http.ListenAndServe(":4000", mux)
