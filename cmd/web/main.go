8cb06e6d85dfd078153f1edd011bd68232f7a417
Otavio Cantareli <otavio_cantareli@outlook.com>
2023-04-28 11:43:12 -0300

    chore: Add flag option
    Added cli flag option to specify port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
 	mux := routes()
 
-	log.Print("Starting on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
